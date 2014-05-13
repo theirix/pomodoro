5025c7e1f484ce8e05330dd5a609db23e30becab
Valerii Hiora <valerii.hiora@gmail.com>
2014-05-13 08:07:57 +0300

    Fixed crash while checking for long break

--- Pomodoro/src/PomodoroController.h
+++ Pomodoro/src/PomodoroController.h
@@ -61,7 +61,7 @@
 	IBOutlet GrowlNotifier* __weak growl;
 	IBOutlet Pomodoro* __weak pomodoro;
     NSInteger longBreakCounter;
-    NSTimer* __weak longBreakCheckerTimer;
+    NSTimer* longBreakCheckerTimer;
     PomodoroNotifier* pomodoroNotifier;
     
     ProcessSerialNumber psn;
@@ -100,7 +100,7 @@
 @property (weak) IBOutlet GrowlNotifier* growl;
 @property (weak) IBOutlet Pomodoro* pomodoro;
 @property (nonatomic, assign) NSInteger longBreakCounter;
-@property (nonatomic, weak) NSTimer* longBreakCheckerTimer;
+@property (nonatomic) NSTimer* longBreakCheckerTimer;
 
 
 @property (nonatomic, weak, readonly) IBOutlet NSMenuItem* startPomodoro;
